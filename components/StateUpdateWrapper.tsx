1efd1bbb572a0b063709e9049d5725cd2649f253
bufan <bufan@hotmail.com>
2024-04-03 13:53:57 +0800

    remvoe extra code

--- components/StateUpdateWrapper.tsx
+++ components/StateUpdateWrapper.tsx
@@ -3,15 +3,6 @@ import { PropsWithChildren, useEffect } from "react";
 import useUserAction from "~/hooks/user/useUserAction";
 import useUserTotalPoints from "~/hooks/user/useUserTotalPoints";
 import useSeenCasts from "~/hooks/user/useSeenCasts";
-import { getUserToken } from "~/services/shared/api/request";
-import { injectPrivyToken } from "~/utils/privy/injectToken";
-
-const waitToken = async () => {
-  const token = getUserToken();
-  if (!token) {
-    await injectPrivyToken();
-  }
-};
 
 export default function StateUpdateWrapper({ children }: PropsWithChildren) {
   const { authenticated } = usePrivy();
@@ -29,7 +20,6 @@ export default function StateUpdateWrapper({ children }: PropsWithChildren) {
   useEffect(() => {
     (async () => {
       if (authenticated) {
-        await waitToken(); // TODO 注入token时机优化后删除
         await submitUnreportedActions();
         await fetchTotalPoints();
       }
@@ -39,7 +29,6 @@ export default function StateUpdateWrapper({ children }: PropsWithChildren) {
   useEffect(() => {
     (async () => {
       if (authenticated) {
-        await waitToken(); // TODO 注入token时机优化后删除
         submitUnreportedViewCasts();
       }
     })();
