8fe43319a079422452413473c50584571c5c3c1f
shixuewen <friendlysxw@163.com>
2024-04-08 18:59:20 +0800

    feat: community layout

---
+++ app/communities/[id]/tips-rank.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
+
+export default function TipsRankScreen() {
+  return (
+    <View>
+      <Text>TipsRank Screen</Text>
+    </View>
+  );
+}
