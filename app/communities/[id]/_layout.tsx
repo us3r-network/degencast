1e8f70352892662c74ba68f48207da94f8cb57b0
shixuewen <friendlysxw@163.com>
2024-04-09 16:04:50 +0800

    feat: community meta info & join button

--- app/communities/[id]/_layout.tsx
+++ app/communities/[id]/_layout.tsx
@@ -4,9 +4,13 @@ import {
   useLocalSearchParams,
   useSegments,
 } from "expo-router";
+import { useEffect } from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import CommunityDetailMetaInfo from "~/components/community/CommunityDetailMetaInfo";
+import CommunityJoinButton from "~/components/community/CommunityJoinButton";
 import { Card } from "~/components/ui/card";
 import { Tabs, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import useLoadCommunityDetail from "~/hooks/community/useLoadCommunityDetail";
 import { cn } from "~/lib/utils";
 
 const initialRouteName = "tokens";
@@ -24,57 +28,71 @@ export default function CommunityDetail() {
   const segments = useSegments();
   const activeScreen = segments[2] || initialRouteName;
   const router = useRouter();
+  const { community, loading, loadCommunity } = useLoadCommunityDetail();
+  useEffect(() => {
+    loadCommunity(id as string);
+  }, [id]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }} className="bg-primary">
       <Stack.Screen
         options={{
+          // headerTransparent: true,
+          headerStyle: {
+            backgroundColor: "transparent",
+          },
           headerTitle: id as string,
           headerRight: () => (
             <View className="mr-5 flex flex-row items-center gap-3">
-              <Text>Join</Text>
+              {community && <CommunityJoinButton communityInfo={community} />}
               <Text>Share</Text>
             </View>
           ),
         }}
       />
-      <View className=" flex-1 p-5">
-        <View className="box-border w-full flex-1 pt-7">
-          <Tabs
-            value={activeScreen}
-            onValueChange={(value) => {
-              router.push(`/communities/${id}/${value}` as any);
-            }}
-            className=" absolute left-1/2 top-0 z-10 box-border w-full -translate-x-1/2"
-          >
-            <TabsList className="flex-row rounded-full bg-white shadow-lg">
-              {TABS.map((tab) => (
-                <TabsTrigger
-                  key={tab.value}
-                  className={cn("flex-1 flex-row rounded-full")}
-                  value={tab.value}
-                >
-                  <Text
-                    className={cn(
-                      "text-primary",
-                      activeScreen === tab.value && "text-primary-foreground",
-                    )}
-                  >
-                    {tab.label}
-                  </Text>
-                </TabsTrigger>
-              ))}
-            </TabsList>
-          </Tabs>
-          <Card className="box-border h-full w-full p-5">
-            <Stack
-              initialRouteName={initialRouteName}
-              screenOptions={{
-                header: () => null,
-              }}
-            />
-          </Card>
-        </View>
+      <View className=" flex-1 flex-col gap-7 p-5">
+        {community && (
+          <>
+            <CommunityDetailMetaInfo communityInfo={community} />
+            <View className="box-border w-full flex-1 pt-7">
+              <Tabs
+                value={activeScreen}
+                onValueChange={(value) => {
+                  router.push(`/communities/${id}/${value}` as any);
+                }}
+                className=" absolute left-1/2 top-0 z-10 box-border w-full -translate-x-1/2"
+              >
+                <TabsList className="flex-row rounded-full bg-white shadow-lg">
+                  {TABS.map((tab) => (
+                    <TabsTrigger
+                      key={tab.value}
+                      className={cn("flex-1 flex-row rounded-full")}
+                      value={tab.value}
+                    >
+                      <Text
+                        className={cn(
+                          "text-primary",
+                          activeScreen === tab.value &&
+                            "text-primary-foreground",
+                        )}
+                      >
+                        {tab.label}
+                      </Text>
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+              </Tabs>
+              <Card className="box-border h-full w-full p-5">
+                <Stack
+                  initialRouteName={initialRouteName}
+                  screenOptions={{
+                    header: () => null,
+                  }}
+                />
+              </Card>
+            </View>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
---
+++ components/community/CommunityJoinButton.tsx
@@ -0,0 +1,51 @@
+import { Text } from "~/components/ui/text";
+import { Button, ButtonProps } from "../ui/button";
+import useJoinCommunityAction from "~/hooks/community/useJoinCommunityAction";
+import { CommunityInfo } from "~/services/community/types/community";
+import { cn } from "~/lib/utils";
+import { TextProps } from "react-native";
+
+export default function CommunityJoinButton({
+  communityInfo,
+  className,
+  textProps,
+  ...props
+}: ButtonProps & {
+  communityInfo: CommunityInfo;
+  textProps?: TextProps;
+}) {
+  const { joined, isPending, joinChangeAction } =
+    useJoinCommunityAction(communityInfo);
+  const { className: textClassName, ...restTextProps } = textProps || {};
+  return (
+    <Button
+      className={cn("w-14 bg-secondary", className)}
+      onPress={(e) => {
+        e.stopPropagation();
+        joinChangeAction();
+      }}
+      {...props}
+    >
+      <Text
+        className={cn(
+          "text-xs font-bold text-secondary-foreground",
+          textClassName,
+        )}
+        {...(restTextProps || {})}
+      >
+        {(() => {
+          if (joined) {
+            if (isPending) {
+              return "Unjoining ...";
+            }
+            return "Joined";
+          }
+          if (isPending) {
+            return "Joining ...";
+          }
+          return "Join";
+        })()}
+      </Text>
+    </Button>
+  );
+}
