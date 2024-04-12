fa0dd4c929001d62d93c8a83b3a741b40776174b
bufan <bufan@hotmail.com>
2024-04-12 17:22:53 +0800

    add loading state

---
+++ components/common/Loading.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export function Loading() {
+  return (
+    <View className="flex h-full w-full items-center justify-center">
+      <Text>Loading...</Text>
+    </View>
+  );
+}
