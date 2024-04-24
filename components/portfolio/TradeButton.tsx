159f6a9e13977bb95273abfbd9de88e5cc5f0ae1
bufan <bufan@hotmail.com>
2024-04-24 19:45:02 +0800

    swap token ui

--- components/portfolio/TradeButton.tsx
+++ components/portfolio/TradeButton.tsx
@@ -1,5 +1,5 @@
 // import { useSwitchChain } from "wagmi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Button } from "~/components/ui/button";
 import {
@@ -69,10 +69,19 @@ function SwapToken({
   const [fromToken, setFromToken] = useState(token1);
   const [toToken, setToToken] = useState(token2);
   const [fromAmount, setFromAmount] = useState("0");
-  const [toAmount, setToAmount] = useState(0);
+  const [toAmount, setToAmount] = useState("0");
+  const fetchTransactionInfo = (amount: string) => {
+    console.log("fetchTransactionInfo", amount);
+    setFromAmount(amount);
+    setToAmount(amount);
+  };
   return (
     <View className="flex w-full gap-2">
-      <Token token={fromToken} />
+      <Token
+        token={fromToken}
+        amount={fromAmount}
+        setAmount={fetchTransactionInfo}
+      />
       <View className="flex-row items-center">
         <Separator className="flex-1 text-secondary" />
         <Button
@@ -86,7 +95,10 @@ function SwapToken({
         </Button>
         <Separator className="flex-1 text-secondary" />
       </View>
-      <Token token={toToken || NATIVE_TOKEN_METADATA} />
+      <Token
+        token={toToken || NATIVE_TOKEN_METADATA}
+        amount={toAmount}
+      />
       <Button
         variant="secondary"
         className="mt-6"
@@ -100,18 +112,27 @@ function SwapToken({
   );
 }
 
-function Token({ token }: { token: TokenInfoWithMetadata }) {
-  const [amount, setAmount] = useState("0");
+function Token({
+  token,
+  amount,
+  setAmount,
+}: {
+  token: TokenInfoWithMetadata;
+  amount?: string;
+  setAmount?: (amount: string) => void;
+}) {
   const price = Number(token.tradeInfo?.stats.token_price_usd) || 0;
   return (
     <View className="flex gap-2">
       <View className="flex-row items-start justify-between">
         <TokenInfo name={token.name} logo={token.logo} />
         <Input
+          editable={!!setAmount}
           className={cn(
             "max-w-40 rounded-full border-none bg-secondary/20 text-end text-white",
           )}
           inputMode="numeric"
+          defaultValue="0"
           value={amount}
           onChangeText={setAmount}
         />
