33a047ecd33bd1e327c330208f9a5be8946dbe65
bufan <bufan@hotmail.com>
2024-04-28 17:01:36 +0800

    add href to user info

--- components/portfolio/user/FarcasterStats.tsx
+++ components/portfolio/user/FarcasterStats.tsx
@@ -1,27 +1,50 @@
 import { View } from "react-native";
+import { ExternalLink } from "~/components/common/ExternalLink";
+import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
 import useFarcasterUserStats from "~/hooks/social-farcaster/useFarcasterUserStats";
 
-export default function FarcasterStats({ fid }: { fid: number }) {
+const WARRPCAST = "https://warpcast.com";
+export default function FarcasterStats({
+  fid,
+  fname,
+}: {
+  fid: number;
+  fname: string;
+}) {
   const { farcasterUserStats } = useFarcasterUserStats(fid);
   return (
-    <View className="w-full flex-row gap-6">
-      <View className="flex-row items-center gap-2">
-        <Text className="text-sm font-bold text-white">
-          {new Intl.NumberFormat("en-US", {
-            notation: "compact",
-          }).format(farcasterUserStats.followingCount)}
-        </Text>
-        <Text className="text-sm  text-secondary">Following</Text>
-      </View>
-      <View className="flex-row items-center gap-2">
-        <Text className="text-sm font-bold text-white">
-          {new Intl.NumberFormat("en-US", {
-            notation: "compact",
-          }).format(farcasterUserStats.followerCount)}
-        </Text>
-        <Text className="text-sm  text-secondary">Followers</Text>
-      </View>
+    <View className="w-full flex-row gap-4">
+      <ExternalLink href={`${WARRPCAST}/${fname}/following`} target="_blank">
+        <Button variant="link" className="flex-row items-center gap-1 p-0 h-6">
+          <Text className="text-sm font-bold text-white">
+            {new Intl.NumberFormat("en-US", {
+              notation: "compact",
+            }).format(farcasterUserStats.followingCount)}
+          </Text>
+          <Text className="text-sm  text-secondary">Following</Text>
+        </Button>
+      </ExternalLink>
+      <ExternalLink href={`${WARRPCAST}/${fname}/followers`} target="_blank" asChild>
+        <Button variant="link" className="flex-row items-center gap-1 p-0 h-6">
+          <Text className="text-sm font-bold text-white">
+            {new Intl.NumberFormat("en-US", {
+              notation: "compact",
+            }).format(farcasterUserStats.followerCount)}
+          </Text>
+          <Text className="text-sm  text-secondary">Followers</Text>
+        </Button>
+      </ExternalLink>
+      <ExternalLink href={`${WARRPCAST}/${fname}`} target="_blank" asChild>
+        <Button variant="link" className="flex-row items-center gap-1 p-0 h-6">
+          <Text className="text-sm font-bold text-white">
+            {new Intl.NumberFormat("en-US", {
+              notation: "compact",
+            }).format(farcasterUserStats.postCount)}
+          </Text>
+          <Text className="text-sm  text-secondary">Casts</Text>
+        </Button>
+      </ExternalLink>
     </View>
   );
 }
