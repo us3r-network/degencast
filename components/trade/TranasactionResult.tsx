63894ddd1a9d09244ec03adb87ab10324d916be6
bufan <bufan@hotmail.com>
2024-04-29 15:45:19 +0800

    transaction feed back

---
+++ components/trade/TranasactionResult.tsx
@@ -0,0 +1,89 @@
+import { Link } from "expo-router";
+import { View } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+import { SHARE_CONTRACT_CHAIN } from "~/hooks/trade/useShareContract";
+import { Check, Cross } from "../common/Icons";
+import { TransactionReceipt } from "viem";
+import { ReactNode } from "react";
+
+export type TransationData = {
+  transactionReceipt: TransactionReceipt;
+  from: ReactNode;
+  to: ReactNode;
+  description: string;
+};
+
+type TransactionInfoProps = React.ComponentPropsWithoutRef<typeof View> & {
+  data: TransationData;
+  buttonText: string;
+  buttonAction?: () => void;
+};
+
+export function TransactionSuccessInfo({
+  data,
+  buttonText,
+  buttonAction,
+}: TransactionInfoProps) {
+  console.log("TransactionSuccessInfo", data);
+  return (
+    <View className="flex items-center gap-6">
+      <View className="size-16 items-center justify-center rounded-full bg-[green]/40">
+        <Check className="size-8 text-[green]" />
+      </View>
+      <Text className="font-bold">{data.description}</Text>
+      <View className="flex items-center gap-1">
+        <View>{data.from}</View>
+        <Text className="text-secondary">to</Text>
+        <View>{data.to}</View>
+      </View>
+      <View className="w-full flex-row justify-items-stretch gap-4">
+        <Link
+          className="flex-1/2 text-primary-foreground/80"
+          href={`${SHARE_CONTRACT_CHAIN.blockExplorers.default.url}/tx/${data.transactionReceipt.transactionHash}`}
+          target="_blank"
+        >
+          <Button variant="outline" className="w-full border-secondary">
+            <Text className="text-secondary">See Details</Text>
+          </Button>
+        </Link>
+        <Button
+          variant="secondary"
+          className="flex-1"
+          onPress={() => {
+            buttonAction?.();
+          }}
+        >
+          <Text>{buttonText}</Text>
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+type ErrorInfoProps = React.ComponentPropsWithoutRef<typeof View> & {
+  error: string;
+  buttonText: string;
+  buttonAction?: () => void;
+};
+export function ErrorInfo({ error, buttonText, buttonAction }: ErrorInfoProps) {
+  return (
+    <View className="flex items-center gap-6">
+      <View className="size-16 items-center justify-center rounded-full bg-[red]/40">
+        <Cross className="size-8 text-[red]" />
+      </View>
+      <Text className="font-bold">{error}</Text>
+      <View className="w-full flex-row justify-items-stretch gap-4">
+        <Button
+          variant="secondary"
+          className="flex-1"
+          onPress={() => {
+            buttonAction?.();
+          }}
+        >
+          <Text>{buttonText}</Text>
+        </Button>
+      </View>
+    </View>
+  );
+}
