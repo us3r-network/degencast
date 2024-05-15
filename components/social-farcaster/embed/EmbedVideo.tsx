a24f7169de564a91533538a2217e3bc54baa3aec
shixuewen <friendlysxw@163.com>
2024-05-15 12:42:34 +0800

    feat: video.js

--- components/social-farcaster/embed/EmbedVideo.tsx
+++ components/social-farcaster/embed/EmbedVideo.tsx
@@ -1,18 +1,34 @@
 import { ResizeMode, Video } from "expo-av";
-import { View } from "react-native";
+import { isDesktop } from "react-device-detect";
+import { Platform, View } from "react-native";
+import WebVideoRender from "~/components/common/WebVideoRender";
+import { AspectRatio } from "~/components/ui/aspect-ratio";
 
 export default function EmbedVideo({ uri }: { uri: string }) {
   return (
-    <View className=" h-72 w-full">
-      <Video
-        style={{ flex: 1 }}
-        source={{
-          uri,
-        }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-      />
+    <View className="w-full">
+      <AspectRatio ratio={16 / 9}>
+        {Platform.OS === "web" && isDesktop ? (
+          <WebVideoRender videoSrc={uri} />
+        ) : (
+          <Video
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            videoStyle={{
+              width: "100%",
+              height: "100%",
+            }}
+            source={{
+              uri,
+            }}
+            useNativeControls
+            resizeMode={ResizeMode.CONTAIN}
+            isLooping
+          />
+        )}
+      </AspectRatio>
     </View>
   );
 }
