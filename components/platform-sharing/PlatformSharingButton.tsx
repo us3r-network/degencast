f16764f457a1484435309a466decc0b4fbf84342
shixuewen <friendlysxw@163.com>
2024-05-15 18:28:28 +0800

    feat: header menu

--- components/platform-sharing/PlatformSharingButton.tsx
+++ components/platform-sharing/PlatformSharingButton.tsx
@@ -3,6 +3,20 @@ import { Share2 } from "../common/Icons";
 import { Button, ButtonProps } from "../ui/button";
 import PlatformSharingModal from "./PlatformSharingModal";
 import { useState } from "react";
+import {
+  getAppShareTextWithTwitter,
+  getAppShareTextWithWarpcast,
+  getCommunityShareTextWithTwitter,
+  getCommunityShareTextWithWarpcast,
+} from "~/utils/platform-sharing/text";
+import {
+  getAppFrameLink,
+  getAppWebsiteLink,
+  getCommunityFrameLink,
+  getCommunityWebsiteLink,
+  getTradePageFrameLink,
+  getTradePageWebsiteLink,
+} from "~/utils/platform-sharing/link";
 
 export default function PlatformSharingButton({
   text,
@@ -45,5 +59,58 @@ export default function PlatformSharingButton({
         frameLink={frameLink}
       />
     </>
+  );
+}
+
+export function ExploreSharingButton({ fid }: { fid: string | number }) {
+  return (
+    <PlatformSharingButton
+      twitterText={getAppShareTextWithTwitter()}
+      warpcastText={getAppShareTextWithWarpcast()}
+      websiteLink={getAppWebsiteLink({
+        fid,
+      })}
+      frameLink={getAppFrameLink({
+        fid,
+      })}
+    />
+  );
+}
+
+export function TradeSharingButton({ fid }: { fid: string | number }) {
+  return (
+    <PlatformSharingButton
+      twitterText={getAppShareTextWithTwitter()}
+      warpcastText={getAppShareTextWithWarpcast()}
+      websiteLink={getTradePageWebsiteLink({
+        fid,
+      })}
+      frameLink={getTradePageFrameLink({
+        fid,
+      })}
+    />
+  );
+}
+
+export function CommunitySharingButton({
+  name,
+  channelId,
+  currFid,
+}: {
+  name: string;
+  channelId: string;
+  currFid: string | number;
+}) {
+  return (
+    <PlatformSharingButton
+      twitterText={getCommunityShareTextWithTwitter(name || "")}
+      warpcastText={getCommunityShareTextWithWarpcast(name || "")}
+      websiteLink={getCommunityWebsiteLink(channelId, {
+        fid: currFid,
+      })}
+      frameLink={getCommunityFrameLink(channelId, {
+        fid: currFid,
+      })}
+    />
   );
 }
