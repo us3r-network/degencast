7eb57f03b46e478efbb99037485b376a2c2d1a67
shixuewen <friendlysxw@163.com>
2024-05-16 11:13:02 +0800

    feat: display logo on pc

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Link, Tabs, useRouter } from "expo-router";
 import React, { useEffect } from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Search } from "~/components/common/Icons";
 import {
@@ -76,7 +76,17 @@ export default function TabLayout() {
                 }}
                 className="flex-row items-center justify-between bg-primary"
               >
-                <View>
+                <View className="flex-row items-center gap-5">
+                  <View className="hidden sm:block">
+                    <Image
+                      source={require("~/assets/images/favicon.png")}
+                      style={{
+                        width: 40,
+                        height: 40,
+                        resizeMode: "contain",
+                      }}
+                    />
+                  </View>
                   <Text className=" text-xl text-white">Explore</Text>
                 </View>
                 <View className="flex-row items-center gap-[10px]">
@@ -106,30 +116,47 @@ export default function TabLayout() {
             tabBarLabelPosition: "below-icon",
             tabBarIcon: ({ color }) => <TradeIcon fill={color} />,
             headerTransparent: true,
-            headerTitleStyle: {
-              color: "white",
-              fontSize: 20,
-              fontWeight: "500",
-            },
             headerStyle: {
               height: 54,
             },
-            headerRight: () => (
-              <View className="flex-row items-center gap-[10px] p-4">
-                <UserGlobalPoints />
-                {/* <UserWallets /> */}
-                <Link href="/search" asChild>
-                  <Button variant={"link"} className="m-0 p-0">
-                    <Search className=" h-6 w-6 cursor-pointer stroke-white" />
-                  </Button>
-                </Link>
-                <Link href="/create" asChild>
-                  <Button variant={"link"} className="m-0 p-0">
-                    <EditIcon className=" h-6 w-6 cursor-pointer stroke-white" />
-                  </Button>
-                </Link>
-                <View>
-                  <TradeSharingButton fid={currFid} />
+            header: () => (
+              <View
+                style={{
+                  height: 54,
+                  paddingLeft: 15,
+                  paddingRight: 15,
+                }}
+                className="flex-row items-center justify-between bg-primary"
+              >
+                <View className="flex-row items-center gap-5">
+                  <View className="hidden sm:block">
+                    <Image
+                      source={require("~/assets/images/favicon.png")}
+                      style={{
+                        width: 40,
+                        height: 40,
+                        resizeMode: "contain",
+                      }}
+                    />
+                  </View>
+                  <Text className=" text-xl text-white">Channels</Text>
+                </View>
+                <View className="flex-row items-center gap-[10px]">
+                  <UserGlobalPoints />
+                  {/* <UserWallets /> */}
+                  <Link href="/search" asChild>
+                    <Button variant={"link"} className="m-0 p-0">
+                      <Search className=" h-6 w-6 cursor-pointer stroke-white" />
+                    </Button>
+                  </Link>
+                  <Link href="/create" asChild>
+                    <Button variant={"link"} className="m-0 p-0">
+                      <EditIcon className=" h-6 w-6 cursor-pointer stroke-white" />
+                    </Button>
+                  </Link>
+                  <View>
+                    <TradeSharingButton fid={currFid} />
+                  </View>
                 </View>
               </View>
             ),
@@ -142,28 +169,45 @@ export default function TabLayout() {
             tabBarLabelPosition: "below-icon",
             tabBarIcon: ({ color }) => <PortfolioIcon fill={color} />,
             headerTransparent: true,
-            headerTitleStyle: {
-              color: "white",
-              fontSize: 20,
-              fontWeight: "500",
-            },
             headerStyle: {
               height: 54,
             },
-            headerRight: () => (
-              <View className="flex-row items-center gap-[10px] p-4">
-                <UserGlobalPoints />
-                {/* <UserWallets /> */}
-                <Link href="/search" asChild>
-                  <Button variant={"link"} className="m-0 p-0">
-                    <Search className=" h-6 w-6 cursor-pointer stroke-white" />
-                  </Button>
-                </Link>
-                <Link href="/create" asChild>
-                  <Button variant={"link"} className="m-0 p-0">
-                    <EditIcon className=" h-6 w-6 cursor-pointer stroke-white" />
-                  </Button>
-                </Link>
+            header: () => (
+              <View
+                style={{
+                  height: 54,
+                  paddingLeft: 15,
+                  paddingRight: 15,
+                }}
+                className="flex-row items-center justify-between bg-primary"
+              >
+                <View className="flex-row items-center gap-5">
+                  <View className="hidden sm:block">
+                    <Image
+                      source={require("~/assets/images/favicon.png")}
+                      style={{
+                        width: 40,
+                        height: 40,
+                        resizeMode: "contain",
+                      }}
+                    />
+                  </View>
+                  <Text className=" text-xl text-white">Portfolio</Text>
+                </View>
+                <View className="flex-row items-center gap-[10px]">
+                  <UserGlobalPoints />
+                  {/* <UserWallets /> */}
+                  <Link href="/search" asChild>
+                    <Button variant={"link"} className="m-0 p-0">
+                      <Search className=" h-6 w-6 cursor-pointer stroke-white" />
+                    </Button>
+                  </Link>
+                  <Link href="/create" asChild>
+                    <Button variant={"link"} className="m-0 p-0">
+                      <EditIcon className=" h-6 w-6 cursor-pointer stroke-white" />
+                    </Button>
+                  </Link>
+                </View>
               </View>
             ),
           }}
