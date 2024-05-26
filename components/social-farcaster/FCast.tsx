34802f6ba8bfe7746baf869d7cc40a05ada4a95d
shixuewen <friendlysxw@163.com>
2024-05-26 22:50:27 +0800

    feat: explore cast content limits

--- components/social-farcaster/FCast.tsx
+++ components/social-farcaster/FCast.tsx
@@ -6,19 +6,31 @@ import UserGlobalPoints from "../point/UserGlobalPoints";
 import { cn } from "~/lib/utils";
 import FCastUserInfo from "./FCastUserInfo";
 import FCastEmbeds from "./FCastEmbeds";
+import { getEmbeds } from "~/utils/farcaster/getEmbeds";
+import { useMemo } from "react";
 
 export default function FCast({
   cast,
   farcasterUserDataObj,
   className,
   hidePoints = true,
+  webpageImgIsFixedRatio,
+  viewMoreWordLimits,
   ...props
 }: ViewProps & {
   cast: FarCast;
   farcasterUserDataObj?: { [key: string]: UserData } | undefined;
   hidePoints?: boolean;
+  webpageImgIsFixedRatio?: boolean;
+  viewMoreWordLimits?: number;
 }) {
   const userData = farcasterUserDataObj?.[cast.fid];
+  const embeds = useMemo(() => getEmbeds(cast), [cast]);
+  const hasEmbeds =
+    embeds.imgs.length > 0 ||
+    embeds.casts.length > 0 ||
+    embeds.videos.length > 0 ||
+    embeds.webpages.length > 0;
   return (
     <View
       key={cast.id}
@@ -31,8 +43,15 @@ export default function FCast({
         {!hidePoints && <UserGlobalPoints />}
       </View>
       {/* body - text & embed */}
-      <FCastText cast={cast} farcasterUserDataObj={farcasterUserDataObj} />
-      <FCastEmbeds cast={cast} />
+      <FCastText
+        cast={cast}
+        farcasterUserDataObj={farcasterUserDataObj}
+        viewMoreWordLimits={hasEmbeds ? viewMoreWordLimits : undefined}
+      />
+      <FCastEmbeds
+        cast={cast}
+        webpageImgIsFixedRatio={webpageImgIsFixedRatio}
+      />
     </View>
   );
 }
