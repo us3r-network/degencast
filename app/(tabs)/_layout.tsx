bf8185c01419921cebbd700391589e88906211aa
bufan <bufan@hotmail.com>
2024-05-30 15:41:13 +0800

    fix portfolio not login issue

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -125,12 +125,12 @@ export default function TabLayout() {
                   <UserGlobalPoints />
                   <SearchLink />
                   <PostLink />
-                  <View>
+                  {currFid && farcasterAccount && <View>
                     <PortfolioSharingButton
                       fid={Number(currFid)}
                       fname={farcasterAccount.username || ""}
                     />
-                  </View>
+                  </View>}
                 </HeaderRight>
               </Header>
             ),
