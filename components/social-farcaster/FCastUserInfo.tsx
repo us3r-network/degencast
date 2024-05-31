55073a1c2bd09c093272abb627489fcd08fea363
shixuewen <friendlysxw@163.com>
2024-05-31 16:01:54 +0800

    feat: click on the user information to navigate to the portfolio page

--- components/social-farcaster/FCastUserInfo.tsx
+++ components/social-farcaster/FCastUserInfo.tsx
@@ -1,8 +1,9 @@
-import { View, ViewProps } from "react-native";
+import { Pressable, View, ViewProps } from "react-native";
 import { UserData } from "~/utils/farcaster/user-data";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Text } from "../ui/text";
 import { cn } from "~/lib/utils";
+import { Link, useRouter } from "expo-router";
 
 export default function FCastUserInfo({
   userData,
@@ -11,23 +12,31 @@ export default function FCastUserInfo({
 }: ViewProps & {
   userData: UserData;
 }) {
+  const router = useRouter();
   return (
-    <View
-      className={cn("flex flex-1 flex-row items-center gap-1", className)}
-      {...props}
+    <Pressable
+      onPress={(e) => {
+        e.preventDefault();
+        router.push(`/u/${userData.fid}`);
+      }}
     >
-      <Avatar alt={"Avatar"} className="h-5 w-5 rounded-full object-cover">
-        <AvatarImage source={{ uri: userData?.pfp }} />
-        <AvatarFallback>
-          <Text>{userData?.display.slice(0, 1)}</Text>
-        </AvatarFallback>
-      </Avatar>
-      <Text className="line-clamp-1 text-sm font-normal">
-        {userData?.display}
-      </Text>
-      <Text className=" text-xs font-normal text-secondary">
-        @{userData?.userName}
-      </Text>
-    </View>
+      <View
+        className={cn("flex flex-1 flex-row items-center gap-1", className)}
+        {...props}
+      >
+        <Avatar alt={"Avatar"} className="h-5 w-5 rounded-full object-cover">
+          <AvatarImage source={{ uri: userData?.pfp }} />
+          <AvatarFallback>
+            <Text>{userData?.display.slice(0, 1)}</Text>
+          </AvatarFallback>
+        </Avatar>
+        <Text className="line-clamp-1 text-sm font-normal">
+          {userData?.display}
+        </Text>
+        <Text className=" text-xs font-normal text-secondary">
+          @{userData?.userName}
+        </Text>
+      </View>
+    </Pressable>
   );
 }
