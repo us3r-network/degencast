6bc7d94d06f0a907670b119a0ceef483bac41b84
bufan <bufan@hotmail.com>
2024-06-20 16:32:58 +0800

    change deposit

--- components/trade/DepositButton.tsx
+++ components/trade/DepositButton.tsx
@@ -1,8 +1,21 @@
-import { useConnectWallet, usePrivy, useWallets } from "@privy-io/react-auth";
+import {
+  ConnectedWallet,
+  useConnectWallet,
+  useWallets,
+} from "@privy-io/react-auth";
 import * as Clipboard from "expo-clipboard";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { View } from "react-native";
 import Toast from "react-native-toast-message";
+import {
+  Address,
+  createPublicClient,
+  createWalletClient,
+  custom,
+  formatUnits,
+  http,
+  parseEther,
+} from "viem";
 import { useAccount } from "wagmi";
 import { ArrowDown, Copy } from "~/components/common/Icons";
 import { Button } from "~/components/ui/button";
@@ -14,13 +27,16 @@ import {
   DialogTrigger,
 } from "~/components/ui/dialog";
 import { Text } from "~/components/ui/text";
+import { DEFAULT_CHAIN, NATIVE_TOKEN_METADATA } from "~/constants";
 import { cn } from "~/lib/utils";
+import { shortPubKey } from "~/utils/shortPubKey";
 import UserTokens from "../portfolio/tokens/UserTokens";
+import { Input } from "../ui/input";
+import { Slider } from "../ui/slider";
 import FundButton from "./FundButton";
 
 export default function DepositButton() {
   const { connectWallet } = useConnectWallet();
-  const { linkWallet } = usePrivy();
   const { wallets: connectedWallets } = useWallets();
   const { address: activeWalletAddress } = useAccount();
 
@@ -33,7 +49,16 @@ export default function DepositButton() {
     if (currentWallet) return currentWallet;
   }, [connectedWallets, activeWalletAddress]);
 
-  if (!activeWalletAddress)
+  const connectedExternalWallet = useMemo(() => {
+    // console.log("activeWalletAddress", connectedWallets, activeWalletAddress);
+    if (!connectedWallets?.length) return undefined;
+    const currentWallet = connectedWallets.find(
+      (wallet) => wallet.connectorType !== "embedded",
+    );
+    if (currentWallet) return currentWallet;
+  }, [connectedWallets]);
+
+  if (!activeWalletAddress || !activeWallet)
     return (
       <Button size={"icon"} className="rounded-full" onPress={connectWallet}>
         <Text>
@@ -46,7 +71,10 @@ export default function DepositButton() {
       <Dialog>
         <DialogTrigger
           asChild
-          disabled={activeWallet?.connectorType !== "embedded" && activeWallet?.connectorType !== "coinbase_wallet"}
+          disabled={
+            activeWallet?.connectorType !== "embedded" &&
+            activeWallet?.connectorType !== "coinbase_wallet"
+          }
         >
           <Button size={"icon"} className="rounded-full">
             <Text>
@@ -61,7 +89,7 @@ export default function DepositButton() {
           <View className="flex gap-6">
             <UserTokens address={activeWalletAddress} />
             <View className=" w-full flex-row items-center justify-between gap-2 rounded-lg border-2 border-secondary/50">
-              <Text className="ml-2 text-secondary/50 flex-1 line-clamp-1">
+              <Text className="ml-2 line-clamp-1 flex-1 text-secondary/50">
                 {activeWalletAddress}
               </Text>
               <Button
@@ -78,16 +106,109 @@ export default function DepositButton() {
                 <Copy className="size-4 text-white" />
               </Button>
             </View>
-            <Text className="text-xs">
+            <Text className="text-sm">
               Copy the wallet address and paste it into your crypto wallet to
               transfer. Only sending on Base.
             </Text>
             <FundButton variant="text" />
-            <Button variant="secondary" onPress={linkWallet}>
-              <Text>Link a new wallet & set active</Text>
-            </Button>
+            {!!connectedExternalWallet ? (
+              <TransferFromExternalWallet
+                fromWallet={connectedExternalWallet}
+                toWallet={activeWallet}
+              />
+            ) : (
+              <Button variant="secondary" onPress={connectWallet}>
+                <Text>Connect your wallet & transfer</Text>
+              </Button>
+            )}
           </View>
         </DialogContent>
       </Dialog>
     );
+}
+
+function TransferFromExternalWallet({
+  fromWallet,
+  toWallet,
+}: {
+  fromWallet: ConnectedWallet;
+  toWallet: ConnectedWallet;
+}) {
+  const [amount, setAmount] = useState("0");
+  const [balance, setBalance] = useState(0);
+  useEffect(() => {
+    const publicClient = createPublicClient({
+      chain: DEFAULT_CHAIN,
+      transport: http(),
+    });
+    publicClient
+      .getBalance({
+        address: fromWallet.address as Address,
+      })
+      .then((rawBalance) => {
+        const b = parseFloat(
+          formatUnits(rawBalance, NATIVE_TOKEN_METADATA.decimals!),
+        );
+        setBalance(b);
+        setAmount(String(b / 10));
+      });
+  }, [fromWallet]);
+
+  const transfer = async () => {
+    const value = parseEther(amount);
+    try {
+      const provider = await fromWallet.getEthereumProvider();
+      const client = createWalletClient({
+        chain: DEFAULT_CHAIN,
+        transport: custom(provider),
+      });
+      if (client.chain.id !== DEFAULT_CHAIN.id)
+        await client.switchChain(DEFAULT_CHAIN);
+      const hash = await client.sendTransaction({
+        account: fromWallet.address as Address,
+        to: toWallet.address as Address,
+        value,
+      });
+      Toast.show({
+        type: "success",
+        text1: "Transfer Completeed!",
+        // text2: `Transaction Hash: ${hash}`,
+      });
+    } catch (e: any) {
+      Toast.show({
+        type: "error",
+        text1: "Failed to transfer",
+        // text1: e.message,
+      });
+    }
+  };
+  return (
+    <View className="flex gap-4">
+      <Text className="text-sm">
+        Transfer from connected wallet: {shortPubKey(fromWallet.address)}
+      </Text>
+      <Input
+        className="border-secondary text-secondary"
+        placeholder="Enter amount"
+        value={amount}
+        onChangeText={(newText) => setAmount(newText)}
+      />
+      <Slider
+        value={Number(amount)}
+        max={balance}
+        step={balance / 100}
+        onValueChange={(v) => setAmount(String(v))}
+      />
+      <Button variant="secondary" onPress={() => transfer()}>
+        <Text>
+          Transfer{" "}
+          {new Intl.NumberFormat("en-US", {
+            maximumFractionDigits: 4,
+            notation: "compact",
+          }).format(Number(amount))}{" "}
+          {NATIVE_TOKEN_METADATA.symbol}
+        </Text>
+      </Button>
+    </View>
+  );
 }
