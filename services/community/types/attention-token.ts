a0fdac5505976e9fc1a9621545f062586d0ae93c
shixuewen <friendlysxw@163.com>
2024-06-28 18:59:12 +0800

    feat: activities

---
+++ services/community/types/attention-token.ts
@@ -0,0 +1,14 @@
+export type AttentionTokenEntity = {
+  name: string;
+  logo: string;
+  progress: string;
+  price: number;
+  priceTrend: string;
+  marketCap: number;
+  buy24h: number;
+  sell24h: number;
+  holders: number;
+  tokenStandard: string;
+  tokenContract: string;
+  chain: string;
+};
