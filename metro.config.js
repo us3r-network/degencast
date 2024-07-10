6e25b6e80cd0ae7393fa2f739c13adb2b12a4195
bufan <bufan@hotmail.com>
2024-07-10 12:57:30 +0800

    fix node crypto issue

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,15 @@ const { withNativeWind } = require('nativewind/metro');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.extraNodeModules = {
+    ...require("node-libs-expo"),
+};
+
+config.transformer.getTransformOptions = async () => ({
+    transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+    },
+});
+
 module.exports = withNativeWind(config, { input: './global.css' });
