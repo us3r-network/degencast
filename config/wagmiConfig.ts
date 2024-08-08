c73f801fe40f62194cd0425dffef3e97b5c3f079
bufan <bufan@hotmail.com>
2024-08-08 12:57:11 +0800

    add polygon chain

--- config/wagmiConfig.ts
+++ config/wagmiConfig.ts
@@ -1,4 +1,4 @@
-import { base, baseSepolia, mainnet, sepolia } from "viem/chains";
+import { base, baseSepolia, mainnet, sepolia, polygon } from "viem/chains";
 import { http } from "wagmi";
 
 import { createConfig } from "lib/privy";
@@ -6,11 +6,12 @@ import { createConfig } from "lib/privy";
 // Replace these with your app's chains
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, sepolia, base, baseSepolia],
+  chains: [mainnet, sepolia, base, baseSepolia, polygon],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
     [base.id]: http(),
     [baseSepolia.id]: http(),
+    [polygon.id]: http(),
   },
 });
