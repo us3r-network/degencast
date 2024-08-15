6b4a1d1432ce51864f6ba0b6993af90074fedd1d
bufan <bufan@hotmail.com>
2024-08-15 14:09:44 +0800

    disable preload

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,9 +19,9 @@ import { logGA } from "~/utils/firebase/analytics.web";
 const AUTH_PROTECTED_ROUTES = ["portfolio"];
 export default function TabLayout() {
   useCommunityTokens();
-  useChannelRank();
-  useCuratorRank();
-  useTokenRank();
+  // useChannelRank();
+  // useCuratorRank();
+  // useTokenRank();
   useEffect(() => {
     if (Platform.OS === "web") logGA("app_open", {});
   }, []);
