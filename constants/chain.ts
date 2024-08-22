347fa84eb41253658494e48b724f2cf9d9e5afe2
bufan <bufan@hotmail.com>
2024-08-22 10:34:50 +0800

    add global var and const

--- constants/chain.ts
+++ constants/chain.ts
@@ -7,14 +7,28 @@ export const DEFAULT_CHAINID: number =
   Number(process.env.EXPO_PUBLIC_DEFAULT_CHAIN_ID) || base.id;
 export const DEFAULT_CHAIN = getChain(DEFAULT_CHAINID);
 
-export const NATIVE_TOKEN_ADDRESS:Address = process.env.EXPO_PUBLIC_NATIVE_TOKEN_ADDRESS as Address ||
+export const NATIVE_TOKEN_ADDRESS: Address =
+  (process.env.EXPO_PUBLIC_NATIVE_TOKEN_ADDRESS as Address) ||
   "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"; //ETH
-export const DEGEN_TOKEN_ADDRESS: Address = process.env.EXPO_PUBLIC_DEGEN_TOKEN_ADDRESS as Address ||
+export const WRAP_NATIVE_TOKEN_ADDRESS: Address =
+  (process.env.EXPO_PUBLIC_WRAP_NATIVE_TOKEN_ADDRESS as Address) ||
+  "0x4200000000000000000000000000000000000006"; //ETH
+export const DEGEN_TOKEN_ADDRESS: Address =
+  (process.env.EXPO_PUBLIC_DEGEN_TOKEN_ADDRESS as Address) ||
   "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed"; // Degen
 
 export const NATIVE_TOKEN_METADATA: TokenWithTradeInfo = {
   chainId: DEFAULT_CHAINID,
   address: NATIVE_TOKEN_ADDRESS,
+  name: DEFAULT_CHAIN.nativeCurrency.name,
+  decimals: DEFAULT_CHAIN.nativeCurrency.decimals,
+  symbol: DEFAULT_CHAIN.nativeCurrency.symbol,
+  logoURI: "https://assets.coingecko.com/coins/images/279/small/ethereum.png",
+};
+
+export const WRAP_NATIVE_TOKEN_METADATA: TokenWithTradeInfo = {
+  chainId: DEFAULT_CHAINID,
+  address: WRAP_NATIVE_TOKEN_ADDRESS,
   name: DEFAULT_CHAIN.nativeCurrency.name,
   decimals: DEFAULT_CHAIN.nativeCurrency.decimals,
   symbol: DEFAULT_CHAIN.nativeCurrency.symbol,
