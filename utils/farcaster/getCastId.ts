af3f612daf8fc670f67d6dcb5b161605f2e4885d
bufan <bufan@hotmail.com>
2024-09-02 13:36:19 +0800

    fix compatible issue

--- utils/farcaster/getCastId.ts
+++ utils/farcaster/getCastId.ts
@@ -2,6 +2,7 @@ import { CastId } from "@external-types/farcaster";
 import { FarCast } from "~/services/farcaster/types";
 
 export default function getCastId({ cast }: { cast: FarCast }): CastId {
+  if (!cast.fid || !cast.hash?.data) return { fid: 0, hash: new Uint8Array() };
   return {
     fid: Number(cast.fid),
     hash: Uint8Array.from(cast.hash.data),
