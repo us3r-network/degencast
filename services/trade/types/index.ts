16bfa9267064704ddba169d63aa02b81be210dae
bufan <bufan@hotmail.com>
2024-09-12 11:06:13 +0800

    comment out usd price codes

--- services/trade/types/index.ts
+++ services/trade/types/index.ts
@@ -45,7 +45,6 @@ export type TokenWithTradeInfo = {
   rawBalance?: bigint | string;
   balance?: number | string | undefined;
   channel?: NeynarChannel;
-  usdPrice?: number;
 };
 
 export type ERC42069Token = {
