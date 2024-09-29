b659a7d3a4a8a787b022de75a2836267b14e635f
shixuewen <friendlysxw@163.com>
2024-09-29 10:21:17 +0800

    feat: update allowance cast

--- constants/farcaster.ts
+++ constants/farcaster.ts
@@ -1,8 +1,5 @@
 export const FARCASTER_HUB_URL = process.env.EXPO_PUBLIC_FARCASTER_HUB_URL;
 export const NEYNAR_API_HOST = process.env.EXPO_PUBLIC_NEYNAR_API_HOST;
 export const WARRPCAST = "https://warpcast.com";
-export const ALLOWANCE_CAST_HASH =
-  process.env.EXPO_PUBLIC_ALLOWANCE_CAST_HASH ||
-  "0x64c5c28002edcd4bffb381ccc846b656494d4fd7";
-export const ALLOWANCE_CAST_FID =
-  process.env.EXPO_PUBLIC_ALLOWANCE_CAST_FID || 7061;
+export const ALLOWANCE_CAST_HASH = "0x7de7b9798283f137e2bc8e8bee6f4feb93ab8fa6";
+export const ALLOWANCE_CAST_FID = 222577;
